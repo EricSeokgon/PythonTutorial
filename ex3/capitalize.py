71690b6f088fdd7d720ac3c8836c627749bfec3f
hadesleee <leesk55@gmail.com>
2015-06-05 15:52:02 +0900

    문자 제어 테스트

---
+++ ex3/capitalize.py
@@ -0,0 +1,6 @@
+__author__ = 'Hadeslee'
+print('hello world'.capitalize())
+print('hello world'.upper())
+print('hello world'.__len__())
+print(len('hello world'))
+print('Hello world'.replace('world', 'programming'))
