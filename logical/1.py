0f720241da39e108f97e30d0f9f148eaed914bfd
hadesleee <leesk55@gmail.com>
2015-06-08 16:17:59 +0900

    logical add

---
+++ logical/1.py
@@ -0,0 +1,8 @@
+__author__ = 'sklee'
+in_str = input("입력해주세요.\n")
+real_hadeslee = "hadeslee"
+real_k8805 = "k8805"
+if real_hadeslee == in_str or real_k8805 == in_str:
+    print("Hello!!! "+in_str)
+else:
+    print("Who are you?")
