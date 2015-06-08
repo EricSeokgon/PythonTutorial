613078be3a3ef938da23a1fde0ba8a3574e50aa9
hadesleee <leesk55@gmail.com>
2015-06-08 16:05:17 +0900

    input add

---
+++ intput_output/2.py
@@ -0,0 +1,10 @@
+__author__ = 'sklee'
+in_str = input("입력해주세요.\n")
+real_hadeslee = "11"
+real_k8805 = "ab"
+if real_hadeslee == in_str:
+    print("Hello!!!, hadeslee")
+elif real_k8805 == in_str:
+    print("Hello!!!, k8805")
+else:
+    print("Who are you?")
