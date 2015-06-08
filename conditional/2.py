3235a014166baacee97af44d39d91e60dff80879
hadesleee <leesk55@gmail.com>
2015-06-08 14:56:02 +0900

    if elif 제어문

--- conditional/2.py
+++ conditional/2.py
@@ -1,7 +1,10 @@
 __author__ = 'sklee'
-input = 22
-real = 22
-if real == input:
-    print("Hello!")
+input = 11
+real_hadeslee = 11
+real_k8805 = "ab"
+if real_hadeslee == input:
+    print("Hello!!!, hadeslee")
+elif real_k8805 == input:
+    print("Hello!!!, k8805")
 else:
     print("Who are you?")
