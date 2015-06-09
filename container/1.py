7a716c2124b8b9b63aff3c05c78ea12899ee4c28
hadesleee <leesk55@gmail.com>
2015-06-09 11:04:35 +0900

    container study

---
+++ container/1.py
@@ -0,0 +1,11 @@
+__author__ = 'sklee'
+print(type('haadeslee'))
+name = 'hadeslee'
+print(name)
+print(type(['hadeslee','leezxhe', 'graphittie']))
+names = ['hadeslee','leezxhe', 'graphittie']
+print(names)
+print(names[0])
+hadeslee = ['programmer', 'busan', 34, False]
+hadeslee[1] = 'seoul'
+print(hadeslee)
