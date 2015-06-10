131d2d3058d8590619c2eb2273510892689c8924
hadesleee <leesk55@gmail.com>
2015-06-10 14:23:23 +0900

    list study

--- list/1.py
+++ list/1.py
@@ -2,3 +2,10 @@ __author__ = 'sklee'
 odd = [1,3,5,7,9]
 print(odd)
 print(odd[0])
+a = [1, 2, 3, ['a', 'b', 'c']]
+print(a[0])
+print(a[-1])
+print(a[3])
+
+a = [1, 2, ['a', 'b', ['Life', 'is']]]
+print(a[2][2][0])
