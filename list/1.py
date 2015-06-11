710ee47717b49392d3255efd881cd896e671f814
hadesleee <leesk55@gmail.com>
2015-06-11 10:36:45 +0900

    리스트 정렬

--- list/1.py
+++ list/1.py
@@ -35,3 +35,22 @@ print(a)
 
 del a[1]
 print(a)
+
+#리스트에 요소 추가(append)
+a = [1, 2, 3]
+a.append(4)
+print(a)
+
+a.append([5,6])
+print(a)
+
+#리스트 정렬(sort)
+a=[1, 4, 3, 2]
+a.sort()
+print(a)
+
+a = ['a', 'c', 'b']
+a.sort()
+print(a)
+
+
