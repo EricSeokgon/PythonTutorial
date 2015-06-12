9f09678b0b3c781d9ba248697d41decf479e31ed
hadesleee <leesk55@gmail.com>
2015-06-12 10:00:48 +0900

    커피 자판기

--- conditional/3.py
+++ conditional/3.py
@@ -8,3 +8,16 @@ while treeHit < 10:
     print("나무를 %d번 찍었습니다." %treeHit)
     if treeHit == 10:
         print("나무 넘어갑니다.")
+
+#커피 자판기
+coffee = 10
+money = 300
+while money:
+    print("돈을 받았으니 커피를 줍니다.")
+    coffee = coffee - 1
+    print("남은 커피의 양은 %d 입니다." %coffee)
+    if not coffee:
+        print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+        break
+
+#커피 자판기 고도화
