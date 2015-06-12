35112b76e40a3abba1482c6b33f6831af2222a4f
hadesleee <leesk55@gmail.com>
2015-06-12 09:43:04 +0900

    while study

---
+++ conditional/3.py
@@ -0,0 +1,10 @@
+__author__ = 'sklee'
+
+#while
+
+treeHit = 0
+while treeHit < 10:
+    treeHit = treeHit + 1
+    print("나무를 %d번 찍었습니다." %treeHit)
+    if treeHit == 10:
+        print("나무 넘어갑니다.")
