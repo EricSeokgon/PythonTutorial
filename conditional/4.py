5db2cb4646e6d8988c120a1fb21ef09b3e433c2b
hadesleee <leesk55@gmail.com>
2015-06-12 10:23:18 +0900

    for문 학생 성적 검사

---
+++ conditional/4.py
@@ -0,0 +1,15 @@
+__author__ = 'sklee'
+#for 알아보기
+test_list = ['one', 'two', 'three']
+for i in test_list:
+    print(i)
+#학생 성적 검사
+marks = [90, 25, 67, 45, 80]
+number = 0
+for mark in marks:
+    number = number + 1
+    if mark >= 60:
+        print("%d번 학생은 합격 입니다." %number)
+    else:
+        print("%d번 학생은 불합격 입니다." %number)
+
