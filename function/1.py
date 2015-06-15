6f20eb46b6bdcc9af7f27ef6f86e5c709936ea1b
hadesleee <leesk55@gmail.com>
2015-06-15 10:42:42 +0900

    함수

---
+++ function/1.py
@@ -0,0 +1,21 @@
+__author__ = 'sklee'
+def sum(a, b):
+    return a+b
+
+a = 3
+b = 4
+c = sum(a, b)
+print(c)
+
+def sum2(a, b):
+    result = a + b
+    return result
+
+a =  sum2(3, 5)
+print(a)
+
+def say():
+    return 'HI'
+
+a = say()
+print(a)
