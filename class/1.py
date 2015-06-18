687f6166172e9fd731133bb44bef7363658d6ca7
hadesleee <leesk55@gmail.com>
2015-06-18 11:10:41 +0900

    클래스이용 더하기 계산기 구현

--- class/1.py
+++ class/1.py
@@ -1,17 +1,16 @@
 __author__ = 'sklee'
-result1 = 0
-result2 = 0
-def adder1(num):
-    global result1
-    result1 += num
-    return result1
+class Calculator:
+    def __init__(self):
+        self.result = 0
 
-def adder2(num):
-    global result2
-    result2 += num
-    return result2
+    def adder(self,num):
+        self.result += num
+        return self.result
 
-print(adder1(3))
-print(adder1(4))
-print(adder2(3))
-print(adder2(7))
+cal1 = Calculator()
+cal2 = Calculator()
+
+print(cal1.adder(3))
+print(cal1.adder(4))
+print(cal2.adder(3))
+print(cal2.adder(7))
