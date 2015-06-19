cc2ad9e717dd5b72ef5e2b6f637af65da448f4de
hadesleee <leesk55@gmail.com>
2015-06-20 02:50:53 +0900

    사칙연산 클래스 학습

---
+++ class/4.py
@@ -0,0 +1,20 @@
+__author__ = 'hadeslee'
+
+
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+
+    def sum(a, b):
+        return a + b
+
+a = FourCal()
+a.setdata(4,2)
+print(a.first)
+print(a.second)
+
+b = FourCal()
+b.setdata(3,7)
+print(b.first)
+print(a.first)
