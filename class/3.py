6e47d0750da8ab89a69f95bc462143123888ec5d
hadesleee <leesk55@gmail.com>
2015-06-20 02:40:06 +0900

    클래스 함수 학습2

--- class/3.py
+++ class/3.py
@@ -1,9 +1,13 @@
 __author__ = 'Administrator'
-#클래스 변수
+# 클래스 변수
 class Service:
     secret = "영구는 배꼽이 두 개다."
-    def sum(self, a,b):
+    def setname(self, name):
+     self.name = name
+    def sum(self, a, b):
         result = a + b
-        print("%s + %s = %s 입니다." %(a,b,result))
+        print("%s님 %s + %s = %s 입니다." % (self.name, a, b, result))
+
 pey = Service()
-pey.sum(1,1)
+pey.setname("홍길동")
+pey.sum(1, 1)
