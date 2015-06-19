0bb2f6e8305324bc85f6d4f697643ed5228d6bbb
hadesleee <leesk55@gmail.com>
2015-06-20 02:42:56 +0900

    클래스 __init__ 학습

--- class/3.py
+++ class/3.py
@@ -2,12 +2,11 @@ __author__ = 'Administrator'
 # 클래스 변수
 class Service:
     secret = "영구는 배꼽이 두 개다."
-    def setname(self, name):
+    def __init__(self, name):
      self.name = name
     def sum(self, a, b):
         result = a + b
         print("%s님 %s + %s = %s 입니다." % (self.name, a, b, result))
 
-pey = Service()
-pey.setname("홍길동")
+pey = Service("홍길동")
 pey.sum(1, 1)
