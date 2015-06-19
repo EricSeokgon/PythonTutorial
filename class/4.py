0713c4f87fad9eb99660bf2b5f025d8251dcd56a
hadesleee <leesk55@gmail.com>
2015-06-20 02:56:50 +0900

    사칙연산 클래스 학습

--- class/4.py
+++ class/4.py
@@ -6,15 +6,29 @@ class FourCal:
         self.first = first
         self.second = second
 
-    def sum(a, b):
-        return a + b
+    def sum(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
 
 a = FourCal()
-a.setdata(4,2)
-print(a.first)
-print(a.second)
-
 b = FourCal()
+a.setdata(4,2)
 b.setdata(3,7)
-print(b.first)
-print(a.first)
+print(a.sum())
+print(a.mul())
+print(a.sub())
+print(a.div())
+
+print(b.sum())
+print(b.mul())
+print(b.sub())
+print(b.div())
