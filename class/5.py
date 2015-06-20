b1db8e5e92fbc3323fe20a1edb17d3d276a02b81
hadesleee <leesk55@gmail.com>
2015-06-21 00:23:08 +0900

    초기값 설정

--- class/5.py
+++ class/5.py
@@ -1,7 +1,17 @@
 __author__ = 'hadeslee'
 class HousePark:
     lastname = "박"
+    def setname(self, name):
+        self.fullname = self.lastname + name
+    def travel(self, where):
+        print("%s, %s여행을가다" %(self.fullname, where))
 pey = HousePark()
 pes = HousePark()
-print(pey.lastname)
+pey.setname("응용")
+pey.travel("부산")
+
+print(pey.fullname)
 print(pes.lastname)
+
+
+
