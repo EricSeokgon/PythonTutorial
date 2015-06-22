5cf257832de4f4ee35abfc89017df169e7372fe9
hadesleee <leesk55@gmail.com>
2015-06-22 15:10:59 +0900

    math 모듈 기능 추가

---
+++ module/mod2.py
@@ -0,0 +1,17 @@
+__author__ = 'sklee'
+# mod2.py
+PI = 3.141592
+
+class Math:
+    def solv(self, r):
+        return PI * (r ** 2)
+
+
+def sum(a, b):
+    return a+b
+
+if __name__ == "__main__":
+    print(PI)
+    a = Math()
+    print(a.solv(2))
+    print(sum(PI , 4.4))
