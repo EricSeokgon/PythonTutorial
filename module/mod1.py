9006217bba1856e327fd172fe4a39aad809ffd46
hadesleee <leesk55@gmail.com>
2015-06-22 15:09:21 +0900

    sum 모듈 기능 추가

--- module/mod1.py
+++ module/mod1.py
@@ -2,3 +2,11 @@ __author__ = 'sklee'
 # mod1.py
 def sum(a, b):
     return a + b
+
+def safe_sum(a, b):
+    if type(a) != type(b):
+        print("더할수 있는 것이 아닙니다.")
+        return
+    else:
+        result = sum(a, b)
+    return result
