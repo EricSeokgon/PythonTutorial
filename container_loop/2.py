a4ec75615de5116c4f0aee44ccf81a197e3a79ba
hadesleee <leesk55@gmail.com>
2015-06-25 10:49:13 +0900

    for in 문

---
+++ container_loop/2.py
@@ -0,0 +1,10 @@
+__author__ = 'sklee'
+members = ['hadeslee', 'leezche', 'graphittie', 'skee']
+# i = 0
+# while i < len(members):
+#     print(members[i])
+#     i = i + 1
+
+for member in members:
+    print(member)
+
