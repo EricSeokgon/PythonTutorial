fc3859be3d8afb3529573ec3d145234adc9c0971
hadesleee <leesk55@gmail.com>
2015-06-25 11:29:54 +0900

    반복문 이용 로그인 체킹

---
+++ container_loop/4.py
@@ -0,0 +1,9 @@
+__author__ = 'sklee'
+input_id = input("아이디를 입력해주세요.\n")
+members = ['egoing', 'k8805', 'leezche']
+for member in members:
+    if member == input_id:
+        print('Hello!, '+member)
+        import sys
+        sys.exit()
+print('Who are you?')
