e761c2c6b3522141826bc6779bc2c1b719f9c51c
hadesleee <leesk55@gmail.com>
2015-07-20 14:45:11 +0900

    메소드 추가

--- class/7.py
+++ class/7.py
@@ -1,7 +1,7 @@
 __author__ = 'sklee'
 class Cal(object):
     def __init__(self, v1, v2):
-        print(v1, v2)
+       # print(v1, v2)
         self.v1 = v1
         self.v2 = v2
     def add(self):
