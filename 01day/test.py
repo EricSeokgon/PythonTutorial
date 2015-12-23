73516d720ea24703193e9ec1fcaa52c60afb7b6e
hadesyi <hadesleeq@gmail.com>
2015-12-23 14:55:30 +0900

    python version info

---
+++ 01day/test.py
@@ -0,0 +1,22 @@
+__author__ = 'hadeslee'
+
+print 4 + 5
+print 12 + 32
+print (4 + 5) * 6
+print 4 + 5 * 6
+print 9 / 5
+print 9.0 / 5.0
+print 9 / 5.0
+
+print "Hello"
+
+a = "My name is"
+b = "sklee"
+
+print a + b
+
+
+import sys
+print sys.version
+print
+print sys.version_info
