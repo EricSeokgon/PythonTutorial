56d19e7f6c984779fe3378b75e75bbed9d779f1d
hadesyi <hadesleeq@gmail.com>
2016-01-05 13:10:15 +0900

    사전의 특징

---
+++ dictionaries/1.py
@@ -0,0 +1,10 @@
+__author__ = 'Hadeslee'
+
+member = {'basketball':5, 'soccer':11, 'baseball':9}
+member['volleybal']=7 #새로운 아이템 설정
+print member
+print
+
+member['volleybal']=6 # 변경
+print member
+print member['basketball']
