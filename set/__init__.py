641eba040895dae5d6a2b4f3ba1cf76eeaf966c5
hadesyi <hadesleeq@gmail.com>
2016-01-05 11:37:16 +0900

    set 자료형

---
+++ set/__init__.py
@@ -0,0 +1,12 @@
+__author__ = 'Hadeslee'
+
+print set()
+print set([1, 2, 3, 4, 5])
+print set([1, 2, 3, 2, 3, 4])
+print set('abc')
+print set([(1, 2, 3), (4, 5, 6)])
+
+A = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print len(A)
+print 5 in A
+print 10 not in A
