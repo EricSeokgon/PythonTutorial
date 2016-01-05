0d33b9fe24d2255a41d7d094daca278c13829a16
hadesyi <hadesleeq@gmail.com>
2016-01-05 10:29:38 +0900

    튜플의 연산

---
+++ tuple/test.py
@@ -0,0 +1,13 @@
+__author__ = 'Hadeslee'
+
+t = (12345, 54321, 'hello!')
+u = t, (1, 2, 3, 4, 5)
+print u
+
+t2 = [1, 2, 3]
+u2 = t2, (1, 2, 4)
+print u2
+
+t3 = {1: "abc", 2: "def"}
+u3 = t3, (1, 2, 3)
+print u3
