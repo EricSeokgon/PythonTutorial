e6d05fee76ecfe374f24740dca367f872ca96e21
hadesyi <hadesleeq@gmail.com>
2016-01-06 13:58:52 +0900

    함수의 정의와 호출

---
+++ function/test.py
@@ -0,0 +1,45 @@
+__author__ = 'Hadeslee'
+
+
+def f5(b):
+    b['a'] = 10
+    a = {"a": 1, "b": 2}
+    f5(a)
+    print a
+
+
+def print_menu():
+    print '1. Snack'
+    print '2. Snack'
+    print '3. Snack'
+    return
+
+
+a = print_menu()
+print a
+
+
+def swap(x, y):
+    return y, x
+
+
+a = 10
+b = 20
+print a, b
+print
+
+a, b = swap(a, b)
+print a, b
+print
+
+
+def add(a, b):
+    return a + b
+
+
+c = add(1, 3.4)
+d = add('abc', 'typing')
+e = add(['list'], ['and', 'list'])
+print a
+print b
+print c
