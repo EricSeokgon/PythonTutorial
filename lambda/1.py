35a250eebd8101f8868323ae70d5fb365569c317
hadesyi <hadesleeq@gmail.com>
2016-01-07 09:45:44 +0900

    람다 함수 사용

--- lambda/1.py
+++ lambda/1.py
@@ -12,3 +12,23 @@ print incr(10, 5)
 
 vargs = lambda x, *args: args
 print vargs(1, 2, 3, 4, 5)
+
+
+def f1(x):
+    return x * x + 3 * x - 10
+
+
+def f2(x):
+    return x * x * x
+
+
+def g(func):
+    return [func(x) for x in range(-10, 10)]
+print g(f1)
+print g(f2)
+
+
+def g(func):
+    return [func(x) for x in range(-10, 10)]
+print g(lambda x: x * x + 3 * x - 10)
+print g(lambda x: x * x * x)
