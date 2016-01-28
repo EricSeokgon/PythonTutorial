2296c57830477897d0f4b2e571603b4e5966eca7
hadesyi <hadesleeq@gmail.com>
2016-01-28 11:32:04 +0900

    리스트 정의시 값을 정하지 않고 호출시 리스트 내의 값들을 처리

--- sequence/sequence_list.py
+++ sequence/sequence_list.py
@@ -1,3 +1,19 @@
 squares = [x ** 2 for x in range(10)]
 print squares
 
+stack = [3, 4, 5]
+print stack
+stack.append(6)
+print stack
+stack.append(7)
+print stack
+
+stack=[3, 4, 5, 6, 7]
+stack.pop()
+print stack
+
+stack=[3, 4, 5, 6]
+stack.pop()
+print stack
+stack.pop()
+print stack
