f4263f66cdc79920eb50745220e94bd328d81e0a
hadesyi <hadesleeq@gmail.com>
2016-01-30 03:09:51 +0900

    리턴값이 없는 함수

--- function/1.py
+++ function/1.py
@@ -1,21 +1,38 @@
 __author__ = 'sklee'
+
+
 def sum(a, b):
-    return a+b
+    return a + b
+
 
 a = 3
 b = 4
 c = sum(a, b)
 print(c)
 
+
 def sum2(a, b):
     result = a + b
     return result
 
-a =  sum2(3, 5)
+
+a = sum2(3, 5)
 print(a)
+
 
 def say():
     return 'HI'
 
+
 a = say()
 print(a)
+
+
+def sum2(a, b):
+    print("%d, %d의 합은 %d입니다." % (a, b, a + b))
+
+
+a = 5
+b = 5
+c = sum2(a, b)
+
